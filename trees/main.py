5af6b2a18549c84ff3217e52e51ae8948fbfeb9b
pallandir <pallandir2@gmail.com>
2024-09-17 01:22:55 +0800

    Add new methods to class (training purpose)

--- trees/main.py
+++ trees/main.py
@@ -34,6 +34,35 @@ class Node:
             if any(b != " " for b in branches):
                 print("\t" + "   ".join(branches))
 
+    def get_leaves(self, root):
+        if not root:
+            return []
+        stack = [root]
+        leaves = []
+        while stack:
+            current = stack.pop()
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+            elif not current.left and not current.right:
+                leaves.append(current.value)
+        print(leaves)
+
+    def find_node(self, root, goal):
+        if not root:
+            return []
+        stack = [root]
+        while stack:
+            current = stack.pop()
+            if current.value == goal:
+                return True
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+        return False
+
 
 if __name__ == "__main__":
     root = Node(1)
@@ -42,3 +71,5 @@ if __name__ == "__main__":
     root.left.left = Node(7)
     root.right.right = Node(5)
     root.display_graph(root)
+    root.get_leaves(root)
+    print(root.find_node(root, 40))
